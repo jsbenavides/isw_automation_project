7140f39afdf31047dac0e927da5be4ee2a321297
Jhonn Steven Benavides Saborío <johnbsinfo92@gmail.com>
2025-01-17 20:37:05 -0600

    adding more web actions on the BasePage used on the UTN ISW QA Project

--- automation_project/src/test/java/pages/BasePage.java
+++ automation_project/src/test/java/pages/BasePage.java
@@ -1,7 +1,13 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BasePage {
 
@@ -19,5 +25,55 @@ public class BasePage {
 
     public String getTitle(WebDriver driver){
         return driver.getTitle();
+    }
+
+    public String getCurrentURL(WebDriver driver){
+        return driver.getCurrentUrl();
+    }
+
+    public void waitClickBase(WebDriver driver, int time, String elementXpath){
+        WebDriverWait wait1;
+        wait1 = new WebDriverWait(driver, time);
+
+        wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(elementXpath))).click();
+    }
+
+    public String waitGetTextBase(WebDriver driver, int time, String elementXpath){
+        WebDriverWait wait1;
+        wait1 = new WebDriverWait(driver, time);
+
+        return wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(elementXpath))).getText();
+    }
+
+    public String waitGetAttributeBase(WebDriver driver, int timeout, String elementXpath, String Attribute){
+        WebDriverWait wait1;
+        wait1 = new WebDriverWait(driver, timeout);
+
+        return wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(elementXpath))).getAttribute(Attribute);
+    }
+
+    public String getElementAttribute(WebElement element, String attribute){
+
+        return element.getAttribute(attribute);
+    }
+
+    public String getElementText(WebElement element){
+
+        return element.getText();
+    }
+
+    public void switchToBrowserTabWithTimeout(WebDriver driver, int tabIndex, int timeout){
+        //get window handlers as list
+        List<String> browserTabs = new ArrayList<String>(driver.getWindowHandles());
+
+        try {
+
+            Thread.sleep(timeout);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+        driver.switchTo().window(browserTabs.get(tabIndex));
     }
 }
